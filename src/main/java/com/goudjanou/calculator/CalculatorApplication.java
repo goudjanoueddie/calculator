f8a4096af39f068db1f49ae8c420477a6200cf8c
goudjanoueddie <goudjanoueddie@gmail.com>
2023-09-29 11:27:49 -0400

    Hazelcast removed

--- src/main/java/com/goudjanou/calculator/CalculatorApplication.java
+++ src/main/java/com/goudjanou/calculator/CalculatorApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import com.hazelcast.client.config.ClientConfig;
+//import com.hazelcast.client.config.ClientConfig;
 
 @SpringBootApplication
 @EnableCaching
@@ -13,10 +13,10 @@ public class CalculatorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorApplication.class, args);
 	}
-	@Bean
+	/*@Bean
 	public ClientConfig hazelcastClientConfig(){
 		ClientConfig clientConfig = new ClientConfig();
 		clientConfig.getNetworkConfig().addAddress("hazelcast");
 		return clientConfig;
-	}
+	}*/
 }
