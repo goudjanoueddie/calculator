8d6cf6b1bf4fcd3a2eb799554bc7f91d96a7d378
goudjanoueddie <goudjanoueddie@gmail.com>
2023-10-02 12:14:01 -0400

    removed @Cachebla annotation

--- src/main/java/com/goudjanou/calculator/Calculator.java
+++ src/main/java/com/goudjanou/calculator/Calculator.java
@@ -1,10 +1,10 @@
 package com.goudjanou.calculator;
-import org.springframework.cache.annotation.Cacheable;
+//import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
-    @Cacheable("sum")
+    //@Cacheable("sum")
     public int sum(int a, int b){
         try{
             Thread.sleep(3000);
